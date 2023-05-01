9f97a6676a7ee6a4627f6e19522fd49c287f278f
0xDMR-com <132168493+0xDMR-com@users.noreply.github.com>
2023-05-01 15:50:14 +0100

    Added RepresentingData.py
    This python script is used to represent the numerous different families that are found in the data used as part of this project. As well as print a bar chart to illustrate this data.

---
+++ RepresentingData.py
@@ -0,0 +1,24 @@
+from PIL import Image             # Allow for processing of imagess
+import matplotlib.pyplot as plt   # Used to create visualisations
+
+# Delcaring the Path of the Dataset
+path_root = '/home/dmr/DatasetForExperiments/adware/'
+
+# Gather the information about the dataset, how many images and families
+from keras.preprocessing.image import ImageDataGenerator
+batches = ImageDataGenerator().flow_from_directory(directory=path_root, target_size=(64,64), batch_size=10000)
+
+# Show the number of families of malware in the dataset
+batches.class_indices
+
+
+# Print information about the images themselves to reaffirm findings 
+imgs, labels = next(batches)
+imgs.shape
+labels.shape     # Prints (BatchSize, NumberOfClasses)
+
+# Plot the information about the quantity of samples and families
+classes = batches.class_indices.keys()
+perc = (sum(labels)/labels.shape[0])*100
+plt.xticks(rotation='vertical')          # Plot the bar chart
+plt.bar(classes,perc)                    # Print the resulting bar chart
